040ede5027cb3db10cc034d9d8c909d9254df4b1
emmanueladelartey <adelartey135@gmail.com>
2026-03-28 16:42:08 +0200

    1

---
+++ prime.py
@@ -0,0 +1,12 @@
+import numpy as np
+new_array = np.arange(1, 11)
+
+print(new_array)
+print(new_array.shape)
+print(new_array.dtype)
+
+nums = new_array.reshape(5, 2)
+
+print(nums)
+print(nums)
+print(nums)
